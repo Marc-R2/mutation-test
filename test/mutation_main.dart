6b4ee82fd0f5cb7f3ab0454970e1b4357210a321
Marc Renken <marc.renken@bitbot.eu>
2023-01-16 03:51:59 +0100

    chore(config): Add

---
+++ test/mutation_main.dart
@@ -0,0 +1,25 @@
+import 'diverse_test.dart' as diverse;
+import 'example2_test.dart' as example2;
+import 'example_test.dart' as example;
+import 'html_reporter_test.dart' as html_reporter;
+import 'mutation_test.dart' as mutation;
+import 'parse_xml_test.dart' as parse_xml;
+import 'progress_bar_test.dart' as progress_bar;
+import 'range_test.dart' as range;
+import 'ratings_test.dart' as ratings;
+import 'report_format_test.dart' as report_format;
+import 'string_helpers_test.dart' as string_helpers;
+
+void main () {
+  diverse.main();
+  example2.main();
+  example.main();
+  html_reporter.main();
+  mutation.main();
+  parse_xml.main();
+  progress_bar.main();
+  range.main();
+  ratings.main();
+  report_format.main();
+  string_helpers.main();
+}
